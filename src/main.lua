bc48337f4831be5c97d3f0aae55556d7a1cb1133
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2024-02-10 16:40:22 +0200

    Added the plugin files. 👀

---
+++ src/main.lua
@@ -0,0 +1,26 @@
+events:on("OnClientGameMessage", function(playerid, destination, message)
+    local ReadConfigGameMessages = config:FetchArraySize("removemessages.GameMessages")
+    
+    for i = 1, ReadConfigGameMessages do 
+        local DefaultGameMessages = config:Fetch("removemessages.GameMessages[" .. tostring(i - 1) .. "]")
+    
+        if message == DefaultGameMessages then
+            return false
+        end
+    end
+
+    return true
+end)
+
+function GetPluginAuthor()
+    return "Swiftly Solution"
+end
+function GetPluginVersion()
+    return "v1.0.0"
+end
+function GetPluginName()
+    return "Swiftly Remove Default Messages (LUA)"
+end
+function GetPluginWebsite()
+    return "https://github.com/swiftly-solution/swiftly_removedefaultmessages"
+end
